009c5bff6cc8b6dcde988ce1754fac6673c480b7
Курицын Алексей <al.curitsin@gmail.com>
2021-08-23 20:21:18 +0500

    TXT --done

--- WIN_API/main.cpp
+++ WIN_API/main.cpp
@@ -1,13 +1,23 @@
 #include<Windows.h>
 #include"resource.h"
 
+HWND hEdit1;
+HWND hEdit2;
+
+HWND hStatic2;
+
+CONST UINT TEXT_SIZE = 256;
+
+CHAR sz_text_1[TEXT_SIZE] = { "Какой-то текст..." };
+CHAR sz_text_2[TEXT_SIZE] = { "" };
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	
+
 	//MessageBox(NULL, "New Window in WIN API", "Window WIN API", MB_ICONINFORMATION | MB_YESNOCANCEL);
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, (DLGPROC)DlgProc, 0);
 
 	return 0;
 }
@@ -20,13 +30,32 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(0), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 1, (LPARAM)hIcon);
+
+		hEdit1 = GetDlgItem(hwnd, IDC_EDIT1);
+		hEdit2 = GetDlgItem(hwnd, IDC_EDIT2);
+
+		hStatic2 = GetDlgItem(hwnd, IDC_STATIC2);
+
+		SendMessage(hEdit1, WM_SETTEXT, 0, (LPARAM)sz_text_1);
+
 	}
-		break;
+	break;
 	case WM_COMMAND:
 	{
 		switch (LOWORD(wParam))
 		{
-		case IDOK: MessageBox(NULL, "Press butn 'OK'", "Info", MB_ICONINFORMATION | MB_OK); break;
+		case IDOK:
+		{
+			SendMessage(hEdit1, WM_GETTEXT, (WPARAM)TEXT_SIZE, (LPARAM)sz_text_2);
+			SendMessage(hEdit2, WM_SETTEXT, 0, (LPARAM)sz_text_2);
+
+			SendMessage(hStatic2, WM_SETTEXT, 0, (LPARAM)sz_text_2);
+			//SendMessage(GetDlgItem(hwnd, IDC_STATIC1), WM_SETTEXT, 0, (LPARAM)sz_text_1);
+			SetDlgItemText(hwnd, IDC_STATIC1, sz_text_1);
+		}
+
+		//MessageBox(NULL, "Press butn 'OK'", "Info", MB_ICONINFORMATION | MB_OK);
+		break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 		}
 	}
--- WIN_API/resource.h
+++ WIN_API/resource.h
@@ -9,6 +9,10 @@
 #define IDC_PROGRESS1                   1002
 #define IDC_ANIMATE1                    1003
 #define IDC_HOTKEY1                     1005
+#define IDC_EDIT1                       1006
+#define IDC_EDIT2                       1007
+#define IDC_STATIC1                     1008
+#define IDC_STATIC2                     1009
 
 // Next default values for new objects
 // 
@@ -16,7 +20,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
