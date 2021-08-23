820df7609af98273dbc3d9be5fb2287acd266811
Курицын Алексей <al.curitsin@gmail.com>
2021-08-23 20:57:18 +0500

    ListBox --done

--- WIN_API/main.cpp
+++ WIN_API/main.cpp
@@ -1,13 +1,22 @@
 #include<Windows.h>
 #include"resource.h"
 
+CONST CHAR* LIST_CONTENT[] =
+{
+	"This",
+	"is",
+	"my",
+	"first",
+	"ListBox"
+};
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	
+
 	//MessageBox(NULL, "New Window in WIN API", "Window WIN API", MB_ICONINFORMATION | MB_YESNOCANCEL);
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, (DLGPROC) DlgProc, 0);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, (DLGPROC)DlgProc, 0);
 
 	return 0;
 }
@@ -20,13 +29,33 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(0), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 1, (LPARAM)hIcon);
+
+		HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+
+		for (int i = 0; i < sizeof(LIST_CONTENT) / sizeof(LIST_CONTENT[0]); i++)
+		{
+			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)LIST_CONTENT[i]);
+		}
 	}
-		break;
+	break;
 	case WM_COMMAND:
 	{
 		switch (LOWORD(wParam))
 		{
-		case IDOK: MessageBox(NULL, "Press butn 'OK'", "Info", MB_ICONINFORMATION | MB_OK); break;
+		case IDOK:
+		{
+			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+
+			int i = SendMessageA(hList, LB_GETCURSEL, 0, 0);
+
+			//MessageBox(NULL, LIST_CONTENT[i], "Вы выбрали", MB_ICONINFORMATION | MB_OK);
+			
+			CHAR sz_buffer[256] = {};
+			SendMessageA(hList, LB_GETTEXT, 0, (LPARAM)sz_buffer);
+			MessageBox(NULL, sz_buffer, "Вы выбрали", MB_ICONINFORMATION | MB_OK);
+		}
+		break;
+
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 		}
 	}
--- WIN_API/resource.h
+++ WIN_API/resource.h
@@ -9,6 +9,8 @@
 #define IDC_PROGRESS1                   1002
 #define IDC_ANIMATE1                    1003
 #define IDC_HOTKEY1                     1005
+#define IDC_TREE1                       1006
+#define IDC_LIST1                       1007
 
 // Next default values for new objects
 // 
@@ -16,7 +18,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
