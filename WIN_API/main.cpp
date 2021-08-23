d0d8c0ebd1d7af8e9d7d53a0fe53f48bff56132a
Курицын Алексей <al.curitsin@gmail.com>
2021-08-23 13:21:28 +0500

    Add project files.

---
+++ WIN_API/main.cpp
@@ -0,0 +1,37 @@
+#include<Windows.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	
+	//MessageBox(NULL, "New Window in WIN API", "Window WIN API", MB_ICONINFORMATION | MB_YESNOCANCEL);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
+
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(0), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 1, (LPARAM)hIcon);
+	}
+		break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDOK: MessageBox(NULL, "Press butn 'OK'", "Info", MB_ICONINFORMATION | MB_OK); break;
+		case IDCANCEL: EndDialog(hwnd, 0); break;
+		}
+	}
+	break;
+	case WM_CLOSE: EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
